3c080a5d4f065a14f70a738e0d01773ca5f7509b
martynalesniak <01171248@pw.edu.pl>
2025-06-01 17:19:44 +0200

    added api calls musibrainz

--- etl/transform/api_calls.py
+++ etl/transform/api_calls.py
@@ -1,8 +1,8 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import pandas as pd
-import os
 from dotenv import load_dotenv 
+import musicbrainzngs
 
 def get_track_id(sp, track_name, artist_name):
     """Takes track_name and artist and returns track_id"""
@@ -18,20 +18,26 @@ def get_track_id(sp, track_name, artist_name):
     return track_id
 
 
-def get_audio_features(sp, track_ids):
-    """Takes a list of max 100 track_ids and returns a dataframe with audio_features for each id"""
-    audio_features_extracted = sp.audio_features(track_ids)
-    audio_features_cleaned = []
-    for track in audio_features_extracted:
-        if track is None:
-            continue
-        for field in ['uri', 'track_href', 'analysis_url', 'time_signature']:
-            track.pop(field, None)
-        audio_features_cleaned.append(track)
-    return pd.DataFrame(audio_features_cleaned)
+def get_duration(sp, track_ids):
+    """Takes a list of track_ids and returns a dataframe with duration for each id"""
+    results = []
+    try:
+        response = sp.tracks(track_ids)
+        for track in response.get('tracks', []):
+            results.append({
+                'track_id': track['id'],
+                'duration_ms': track.get('duration_ms')
+            })
+    except Exception as e:
+        for track_id in track_ids:
+            results.append({
+                'track_id': track_id,
+                'duration_ms': None
+            })
 
+    return pd.DataFrame(results)
 def get_genre(sp, track_ids):
-    "Takes a list of track_ids and returns a dataframe with list of genres for each id "
+    """Takes a list of track_ids and returns a dataframe with list of genres for each id """
     results = []
     for track_id in track_ids:
         try:
@@ -50,4 +56,33 @@ def get_genre(sp, track_ids):
     return pd.DataFrame(results)
 
 
+musicbrainzngs.set_useragent("MyCoolApp", "0.1", "mojemail@example.com")
+def get_artist_area(artist_name):
+    """
+    Search for artist by name and returns his country.
+    """
+    try:
+        result = musicbrainzngs.search_artists(artist=artist_name, limit=15)
+        artists = result.get('artist-list', [])
+        if not artists:
+            print("Artysta nie znaleziony")
+            return None
+        
+        matching_artist = next((a for a in artists if a.get('name', '').lower() == artist_name.lower()), None)
 
+        if not matching_artist:
+            print(f"Brak dokładnego dopasowania dla: {artist_name}")
+            return None
+        
+        mbid = matching_artist['id']
+        artist_data = musicbrainzngs.get_artist_by_id(mbid)
+        artist = artist_data.get("artist", {})
+        # print(f"Nazwa:        {artist.get('name', 'brak')}")
+        # print(f"Kraj (area):  {artist.get('area', {}).get('name', 'brak')}")
+        # print(f"MBID:         {artist.get('id', 'brak')}")
+        
+        area = artist.get('area', {})
+        return area.get('name', None)
+
+    except Exception as e:
+        return None
